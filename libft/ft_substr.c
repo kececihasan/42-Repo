cbc58659544a4afba3ae6aedff192382f84c50ee
Hasan Kececi <hkececi@k1m35s01.42istanbul.com.tr>
2022-02-16 19:24:43 +0300

    yeni

---
+++ libft/ft_substr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkececi <hkececi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/16 19:01:21 by hkececi           #+#    #+#             */
+/*   Updated: 2022/02/16 19:22:36 by hkececi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+//#include <stdio.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*new_str;
+	size_t	i;
+	size_t	j;
+
+	new_str = (char *)malloc(len + 1);
+	if (!s || !new_str)
+		return (0);
+	i = start;
+	j = 0;
+	while (i < ft_strlen(s) && j < len)
+		new_str[j++] = s[i++];
+	new_str[j] = '\0';
+	return (new_str);
+}
+/*int main()
+{
+	char a[] = "Hasan Kececi";
+	printf("%s",ft_substr(a,3,8);
+}*/
