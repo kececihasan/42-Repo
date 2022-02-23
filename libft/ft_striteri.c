2ba8c03ff09d2b5b77ea29e28f67d3a43b4fc05f
Hasan Kececi <hkececi@k1m41s01.42istanbul.com.tr>
2022-02-23 15:55:13 +0300

    yeni

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -1,45 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hkececi <42istanbul.com.tr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/22 17:40:54 by hkececi           #+#    #+#             */
-/*   Updated: 2022/02/23 15:12:40 by hkececi          ###   ########.tr       */
+/*   Created: 2022/02/23 15:13:50 by hkececi           #+#    #+#             */
+/*   Updated: 2022/02/23 15:50:23 by hkececi          ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 //#include <stdio.h>
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-	char	*str;
-	int		i;
+	unsigned int	i;
 
 	if (!s || !f)
-		return (0);
-	str = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
-	if (!str)
-		return (0);
-	i = 0;
-	while (s[i])
 	{
-		str[i] = f(i, s[i]);
-		++i;
+	i = 0;
+		while (s[i])
+		{
+			f(i, s + i);
+			i++;
+		}
 	}
-	str[i] = '\0';
-	return (str);
 }
-/*char f(unsigned int a, char b)
+/*void	f(unsigned int a, char *b)
 {
-	if (b >= 97 && b <= 122)
-		return (b -= 32);
-	return (b);
+	printf("f a: %d and adress s[i]: %s\n",a, b);
 }
 int main()
 {
-	char c[] = "Hasan";
-	printf("%s",ft_strmapi(c, f));
+	char c[20] = "adresligezi";
+	ft_striteri(c,f);
+	printf("%s\n", c);
+
+	return (0);
 }*/
